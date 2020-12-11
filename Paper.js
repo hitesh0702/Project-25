f103e0c87016ce9e3537937d97b2dad90decbeed
hitesh0702 <73782583+hitesh0702@users.noreply.github.com>
2020-12-11 17:25:14 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,25 @@
+class Paper {
+    constructor(x, y, radius) {
+      var options = {
+        isStatic: false,
+        'restitution':0.4,
+        'friction':0.4,
+        'density':1.2
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius
+      
+      this.image=loadImage("paper.png")
+      World.add(world, this.body);
+      }
+    display(){
+      var pos = this.body.position;
+      
+      fill("white");
+      
+      imageMode(CENTER);
+      image(this.image,pos.x,pos.y,33,33);
+  
+     
+    }
+    };
